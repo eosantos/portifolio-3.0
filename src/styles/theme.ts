c66312f8f1485672b65d671514bf867332dc1475
Eduardo Oliveira <deployment@eqseed.com>
2025-05-23 17:24:10 -0300

    feat: atualizando theme.ts.

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,12 +1,34 @@
 // src/styles/theme.ts
 export const lightTheme = {
-  background: '#fff',
-  text: '#121212'
+  background: '#f8f8f2',
+  text: '#282a36',
+  currentline: '#44475a',
+  selection: '#44475a',
+  foreground: '#f8ff2',
+  comment: '#6272a4',
+  can: '#8be9fd',
+  geen: '#50fa7b',
+  orange: '#ffb86c',
+  pink: '#ff79c6',
+  purple: '#bd93f9',
+  red: '#ff5555',
+  yellow: '#f1fa8c'
 };
 
 export const darkTheme = {
-  background: '#121212',
-  text: '#fff'
+  background: '#282a36',
+  text: '#f8f8f2',
+  currentline: '#44475a',
+  selection: '#44475a',
+  foreground: '#f8ff2',
+  comment: '#6272a4',
+  can: '#8be9fd',
+  geen: '#50fa7b',
+  orange: '#ffb86c',
+  pink: '#ff79c6',
+  purple: '#bd93f9',
+  red: '#ff5555',
+  yellow: '#f1fa8c'
 };
 
 export type ThemeType = typeof lightTheme;

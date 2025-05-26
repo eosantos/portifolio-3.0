98520141a93728ea8e502824ee2d0839614396f7
Eduardo Oliveira <eos.eduardooliveira@gmail.com>
2025-05-26 11:39:44 -0300

    fix: correção de caminho global.

--- src/sections/Home/index.tsx
+++ src/sections/Home/index.tsx
@@ -1,8 +1,8 @@
 'use client';
 
+import PerfilImage from '@/components/PerfilImage';
+import { media } from '@/styles/media';
 import styled from 'styled-components';
-import PerfilImage from '../../components/PerfilImage';
-import { media } from '../../styles/media';
 
 const Container = styled.section`
   padding: 4rem 2rem;
